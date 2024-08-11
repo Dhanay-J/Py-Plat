b3787e1673d86910823b0dba0f3a7dc9e902d1ce
Dhanay J <jdhanay13@gmail.com>
2024-08-11 22:34:34 +0530

    Adding Test Cases

--- src/src/Pages/ChallengeView.jsx
+++ src/src/Pages/ChallengeView.jsx
@@ -1,38 +1,110 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import Editor from "@monaco-editor/react";
 
 function getChallenge(eventId, challengeId) {
   return {
     title: `Event ${eventId} | Challenge ${challengeId}`,
-    description: "how to create a web platform it should have",
-    inputF: "A B C D",
-    outputF: "A B",
+    description: "Slice the list to get the first two elements. If the list has less than two elements, return the original array.It should take a list as input and return a list as output. Name of the function should be 'solution'.",
+    inputF: "[3, 5, 6, 7]",
+    outputF: "[3, 5]",
+    testCases:[
+      {input:'[1,2,3,4]', output:'[1, 2]'},
+      {input:'[4,3,2,1]', output:'[4, 3]'},
+      {input:'[1,2]', output:'[1, 2]'},
+      {input:'[1]', output:'[1]'},
+      {input:'[]', output:'[]'},
+    ],
+    defaultCode:"def solution(input_data):\n    # Your code here\n",
+    testCode:{
+      usePrint:false,
+      function:"soulution",
+      functionArgs:["test_input"],
+    }
   };
 }
+
+
+
 
 const ChallengeView = () => {
   const { eventId, challengeId } = useParams();
 
+  const [challenge, setChallenge] = useState(null);
   const [userCode, setUserCode] = useState("    ");
+  const [result, setResult] = useState("");
+  let testSnippet = "";
 
-  const preDefinedCode = [
-    "def solution(input_data):",
-    "    # Your code here",
-    "    ",
-    "    return result",
-    "",
-    "# Test your solution",
-    "print(solution(test_input))",
-  ];
+  const fetchResult = async (code) => {
+    try {
+      const response = await fetch('http://localhost:2000/api/v2/execute', {
+        method: 'POST',
+        headers: {
+          'Content-Length':`${code.length}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          language: 'python',
+          version: '3.12.0',
+          files:[{name: 'main.py', content: code}]
+         }),
+      });
 
-  const challenge = getChallenge(eventId, challengeId);
+      if (!response.ok) {
+        setResult(`HTTP error ! status: ${response.status}`);
+      }
+
+      const responseObj = await response.json();
+      if (responseObj['run']['signal']){
+        const signal = responseObj['run']['signal'];
+          setResult('Time limit exceeded');
+          return;
+      }
+      // console.log(responseObj['run']['output']);
+      setResult(responseObj['run']['output']);
+      
+    } catch (error) {
+      setResult(error.toString());
+  
+      // console.log(error.toString());
+    }finally{
+      return result;
+    }
+  };
+
+  const runTest = async () => { 
+    for (let i = 0; i < challenge['testCases'].length; i++){
+      const test = challenge['testCases'][i];
+      console.log(test);
+
+      const code = userCode + `\nprint(solution(${test.input}))`;
+      console.log(code);
+      const ans = await fetchResult(code);
+      
+      if (result.trim() !== test.output){
+        console.log(ans,'|', test.output);
+        document.getElementsByClassName(`test_${i}`)[0].style.backgroundColor = 'red';
+        return {status: false, message: `Test case ${i+1} failed`}
+      }else{
+        document.getElementsByClassName(`test_${i}`)[0].style.backgroundColor = 'green';
+      }
+    }
+    return {status: true, message: 'All test cases passed'}
+  }
+
+  useEffect(() => {
+    setChallenge(getChallenge(eventId, challengeId));
+  }, [challengeId, eventId]);
 
   return (
     <Container>
       <Row>
         <Col>
-          <Card>
+          {challenge !== null ? 
+          
+          <div>
+            <Card>
             <Card.Header>{challenge.title}</Card.Header>
             <Card.Body>
               <Card className="m-2 p-2">
@@ -42,75 +114,70 @@ const ChallengeView = () => {
               <Card className="m-2 p-2">
                 Output Format : {challenge.outputF}
               </Card>
-              <Form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  let code = "";
-                  for (let i = 0; i < e.target.elements.length; i++) {
-                    if(i!=1){
-                      code += e.target.elements[i].value + "\n";
-                    }
-                  }
-                  console.log(code);
-                } }
-              >
-                <Form.Group>
-                  <Form.Label className="m-2 p-2">Python IDE</Form.Label>
-                  <div
-                    className="border p-2"
-                    style={{ fontFamily: "monospace" }}
-                  >
-                    {preDefinedCode.map((line, index) => (
-                      <div key={index}>
-                        {index === 2 ? (
-                          <Form.Control
-                            as="textarea"
-                            rows={3}
-                            onKeyDown={(e) => {
-                              if (e.key === "Tab") {
-                                e.preventDefault();
-                                const start = e.target.selectionStart;
-                                const end = e.target.selectionEnd;
-                                setUserCode(
-                                  userCode.substring(0, start) +
-                                    "    " +
-                                    userCode.substring(end)
-                                );
-                              }
-                            }}
-                            // defaultValue={'    '}
-                            value={userCode}
-                            onChange={(e) => setUserCode(e.target.value)}
-                            style={{
-                              width: "100%",
-                              border: "none",
-                              borderBottom: "1px solid #ced4da",
-                              resize: "vertical",
-                              fontFamily: "inherit",
-                            }}
-                          />
-                        ) : (
-                          <Form.Control
-                            plaintext
-                            readOnly
-                            defaultValue={line}
-                            style={{
-                              fontFamily: "inherit",
-                              padding: "0.375rem 0.75rem",
-                              marginBottom: "0.25rem",
-                            }}
-                          />
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                </Form.Group>
-                <Button className="m-2 p-2" variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
             </Card.Body>
+
+            <div className="Editor p-1 border m-2 rounded">
+              <Editor
+                height="30vh"
+                defaultLanguage="python"
+                defaultValue={challenge.defaultCode}
+                onChange={(value) => {
+                  setUserCode(value);
+                }}
+              />
+            </div>
+            <Button
+              variant="primary"
+              className="m-2 p-2"
+              onClick={() => {
+                runTest();
+              }}
+            >
+              Submit
+            </Button>
+
+            <Card.Body>
+              
+              {challenge['testCases'].map((test, index) => 
+
+                (
+                <Card className="m-2 p-2">
+                  <Col>
+                    <Row className="m-2">
+                      <Col>
+                        Input : {test.input}
+                      </Col>
+                      <Col>
+                        Output : {test.output}
+                      </Col>
+                    </Row>
+                  </Col>
+
+                  <Col>
+                    <Row className={`border p-2 rounded m-2 test_${index}`}>
+                      <Col className="col-4">
+                        Your Output
+                      </Col>
+                      <Col>
+                          hai
+                      </Col>
+                    </Row>
+                  </Col>
+                </Card>
+                ))
+              }
+              
+            </Card.Body>
+
+
           </Card>
+          </div>
+          : 
+          
+          <div className="m-3">
+            Loading...
+          </div>
+          }
         </Col>
       </Row>
     </Container>
