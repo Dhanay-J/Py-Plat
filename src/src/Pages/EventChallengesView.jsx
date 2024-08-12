cb314e607765f92afd5fa7057a4e1ae9946fbf73
Dhanay J <jdhanay13@gmail.com>
2024-08-12 21:32:14 +0530

    Simple Validation Upon Submit

--- src/src/Pages/EventChallengesView.jsx
+++ src/src/Pages/EventChallengesView.jsx
@@ -3,65 +3,73 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 import { useParams, Link } from "react-router-dom";
 
+// const Challanges = [
+//   {
+//     title: "Challange 1",
+//     description:
+//       "how to create a web platform it should have \
+// 1. Admin page \
+// 2. New users can create account (with only username and a password) \
+// 3. The admin can create an event entity \
+// 4. Users can get into a specific event \
+// 5. User are provided with set of challenges \
+// 6. They can code in a ide in the challenge page and execute their code (python only)\
+// 7. There must be a chat session for a event where users can send their messages \
+// 8. Admin and the user who put the message can delete it",
+//     id: 1,
+//   },
+//   {
+//     title: "Challange 2",
+//     description:
+//       "how to create a web platform it should have \
+// 1. Admin page \
+// 2. New users can create account (with only username and a password) \
+// 3. The admin can create an event entity \
+// 4. Users can get into a specific event \
+// 5. User are provided with set of challenges \
+// 6. They can code in a ide in the challenge page and execute their code (python only)\
+// 7. There must be a chat session for a event where users can send their messages \
+// 8. Admin and the user who put the message can delete it",
+//     id: 2,
+//   },
+//   {
+//     title: "Challange 3",
+//     description:
+//       "how to create a web platform it should have \
+// 1. Admin page \
+// 2. New users can create account (with only username and a password) \
+// 3. The admin can create an event entity \
+// 4. Users can get into a specific event \
+// 5. User are provided with set of challenges \
+// 6. They can code in a ide in the challenge page and execute their code (python only)\
+// 7. There must be a chat session for a event where users can send their messages \
+// 8. Admin and the user who put the message can delete it",
+//     id: 3,
+//   },
+//   {
+//     title: "Challange 4",
+//     description:
+//       "how to create a web platform it should have \
+// 1. Admin page \
+// 2. New users can create account (with only username and a password) \
+// 3. The admin can create an event entity \
+// 4. Users can get into a specific event \
+// 5. User are provided with set of challenges \
+// 6. They can code in a ide in the challenge page and execute their code (python only)\
+// 7. There must be a chat session for a event where users can send their messages \
+// 8. Admin and the user who put the message can delete it",
+//     id: 4,
+//   },
+// ];
+
 const Challanges = [
   {
-    title: "Challange 1",
-    description:
-      "how to create a web platform it should have \
-1. Admin page \
-2. New users can create account (with only username and a password) \
-3. The admin can create an event entity \
-4. Users can get into a specific event \
-5. User are provided with set of challenges \
-6. They can code in a ide in the challenge page and execute their code (python only)\
-7. There must be a chat session for a event where users can send their messages \
-8. Admin and the user who put the message can delete it",
-    id: 1,
-  },
-  {
-    title: "Challange 2",
-    description:
-      "how to create a web platform it should have \
-1. Admin page \
-2. New users can create account (with only username and a password) \
-3. The admin can create an event entity \
-4. Users can get into a specific event \
-5. User are provided with set of challenges \
-6. They can code in a ide in the challenge page and execute their code (python only)\
-7. There must be a chat session for a event where users can send their messages \
-8. Admin and the user who put the message can delete it",
-    id: 2,
-  },
-  {
-    title: "Challange 3",
-    description:
-      "how to create a web platform it should have \
-1. Admin page \
-2. New users can create account (with only username and a password) \
-3. The admin can create an event entity \
-4. Users can get into a specific event \
-5. User are provided with set of challenges \
-6. They can code in a ide in the challenge page and execute their code (python only)\
-7. There must be a chat session for a event where users can send their messages \
-8. Admin and the user who put the message can delete it",
-    id: 3,
-  },
-  {
-    title: "Challange 4",
-    description:
-      "how to create a web platform it should have \
-1. Admin page \
-2. New users can create account (with only username and a password) \
-3. The admin can create an event entity \
-4. Users can get into a specific event \
-5. User are provided with set of challenges \
-6. They can code in a ide in the challenge page and execute their code (python only)\
-7. There must be a chat session for a event where users can send their messages \
-8. Admin and the user who put the message can delete it",
-    id: 4,
-  },
-];
-
+      title: 'Challange 1',
+      description: 'Hello World'
+, id : 1 
+      
+}
+]
 function getChallanges(id) {
   return Challanges;
 }
@@ -84,7 +92,7 @@ const EventChallengesView = () => {
             <Card.Body>
               {allChallanges.map((challenge, index) => (
                 <Link
-                  to={`/event/${eventId}/challenge/${challenge["id"]}`}
+                  to={`/event/${eventId}/challenge/${challenge["id"]}` } style={{textDecoration:'none'}}
                   className="p-1 col-3"
                 >
                   <Row
