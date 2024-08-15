dc96153464c6bf852f758a11f939e03515f42c94
Dhanay J <jdhanay13@gmail.com>
2024-08-15 13:51:36 +0530

    Added new Links

--- src/src/Pages/Components/Header.jsx
+++ src/src/Pages/Components/Header.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Header = () => {
   const loc = useLocation();
 
   return (
     <>
-      <div>Header</div>
-      {loc.pathname === "/events" ? <div>Events</div> : <div>Challenges</div>}
+      {/* <div>Header</div> */}
+      {/* {loc.pathname === "/events" ? <div>Events</div> : <div>Challenges</div>} */}
+    
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Py-Plat</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/playground">Play Ground</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   );
 };
