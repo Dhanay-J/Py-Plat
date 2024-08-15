4562569c236243d2617872f946290532008fd2bf
Dhanay J <jdhanay13@gmail.com>
2024-08-15 13:50:43 +0530

    Added new routes

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,6 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import EventsView from "./Pages/EventsView";
 import EventChallengesView from "./Pages/EventChallengesView";
 import ChallengeView from "./Pages/ChallengeView";
+import Header from  "./Pages/Components/Header";
+import PlayGround from "./Pages/PlayGround";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+// import 
 // import UserView from './Pages/UserView';
 // import AdminView from './Pages/AdminView';
 // import RankingView from './Pages/RankingView';
@@ -9,13 +14,19 @@ import ChallengeView from "./Pages/ChallengeView";
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<EventsView />} />
         <Route path="/event/:eventId" element={<EventChallengesView />} />
+        <Route path="/e" element={<EventChallengesView />} />
         <Route
           path="/event/:eventId/challenge/:challengeId"
           element={<ChallengeView />}
         />
+        <Route path="/playground" element={<PlayGround />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         {/* <Route path="/user" element={<UserView />} />
         <Route path="/admin" element={<AdminView />} />
         <Route path="/ranking" element={<RankingView />} /> */}
